0bef916f6844d86ae534f2ab53d7050a0d79e9b5
Luisa <luisaferreirass08@gmail.com>
2026-03-24 09:26:20 -0300

    feat: adição das urls

--- apps/properties/urls.py
+++ apps/properties/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("properties", views.CreatePropertyView.as_view()),
+    path("properties", views.ListAllPropertiesView.as_view()),
+    path("properties/id:pk/", views.UpdatePropertyView.as_view()),
+    path("properties/id:pk/", views.DeletePropertyView.as_view()),
+    path("properties/id:pk/", views.GetPropertyView.as_view())
+]
