8e21ef271e3fcaa7efcad195dae5065cdcf539fa
Luisa <luisaferreirass08@gmail.com>
2026-03-26 10:19:38 -0300

    feat: criando novas urls e ajustando erros

--- apps/properties/urls.py
+++ apps/properties/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("properties", views.CreatePropertyView.as_view()),
-    path("properties", views.ListAllPropertiesView.as_view()),
-    path("properties/id:pk/", views.UpdatePropertyView.as_view()),
-    path("properties/id:pk/", views.DeletePropertyView.as_view()),
-    path("properties/id:pk/", views.GetPropertyView.as_view())
+    path("properties/", views.CreatePropertyView.as_view()),
+    path("properties/", views.ListAllPropertiesView.as_view()),
+    path("properties/<int:pk>/manage/", views.UpdatePropertyView.as_view()),
+    path("properties/<int:pk>/manage/", views.DeletePropertyView.as_view()),
+    path("properties/<int:pk>/photos/", views.UploadPhotoPropertyView.as_view()),
+    path("properties/<int:pk>/", views.GetPropertyView.as_view())
 ]
