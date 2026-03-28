5aeb13ce9447a80c7d410d58e34c50358664a1b5
Luisa <luisaferreirass08@gmail.com>
2026-03-28 14:01:50 -0300

    fix: junção de urls

--- apps/properties/urls.py
+++ apps/properties/urls.py
@@ -2,12 +2,8 @@ from django.urls import path
 from .views import property_views, photo_views
 
 urlpatterns = [
-    path("properties/", property_views.CreatePropertyView.as_view()),
-    path("properties/", property_views.ListAllPropertiesView.as_view()),
-    path("properties/<int:pk>/manage/", property_views.UpdatePropertyView.as_view()),
-    path("properties/<int:pk>/manage/", property_views.DeletePropertyView.as_view()),
+    path("properties/", property_views.CreateListPropertyView.as_view()),
+    path("properties/<int:pk>/", property_views.RUDPropertyView.as_view()),
     path("properties/<int:pk>/photos/", photo_views.UploadPhotoPropertyView.as_view()),
-    path("properties/<int:pk>/", property_views.GetPropertyView.as_view()),
-    path("photos/<int:pk>/", photo_views.UpdatePhotoPropertyView.as_view()),
-    path("photos/<int:pk>/", photo_views.DeletePhotoPropertyView.as_view())
+    path("photos/<int:pk>/", photo_views.RUDPhotoPropertyView.as_view()),
 ]
