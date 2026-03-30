d8af7ba66b46af297cdbe7096f6a5976df67b09c
DevlTz <kauazin3339@gmail.com>
2026-03-30 17:23:09 -0300

    feat(users): implement/changes the old /profile/ to /me/ endpoint and JWT auth routes

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,14 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, RegisterUserView
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenBlacklistView)
 
 router = DefaultRouter()
 router.register(r"", UserViewSet, basename="user")
-urlpatterns = router.urls
+urlpatterns = [
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout')
+] + router.urls
+
