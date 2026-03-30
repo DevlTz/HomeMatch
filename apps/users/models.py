279000ccca6960700f2f71b365bfe88b28c9d78a
DevlTz <kauazin3339@gmail.com>
2026-03-30 17:20:40 -0300

    refactor(users): remove username and implement custom UserManager for email auth.

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,26 @@
 # apps/users/models.py
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser need is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser need is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     class UserType(models.TextChoices):
@@ -10,7 +30,7 @@ class User(AbstractUser):
     username = None
     name = models.CharField(max_length=120)
     email = models.EmailField(unique=True)
-    
+    objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
     
@@ -25,7 +45,7 @@ class User(AbstractUser):
     
     
  #   favorites = models.ManyToManyField(
-  #      'properties.Property', 
+  #      'properties.Properties', 
    #     related_name='favorited_by', 
   #      blank=True
   #  )
