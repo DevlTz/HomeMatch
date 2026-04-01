04bf4c6040209ea9e5d67629d78eacf7de9f74b7
DevlTz <kauazin3339@gmail.com>
2026-04-01 17:51:36 -0300

    feat(properties): add reviews endpoints to urls

--- apps/properties/urls.py
+++ apps/properties/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("<int:pk>/", property_views.RUDPropertyView.as_view()),
     path("<int:pk>/photos/", photo_views.UploadPhotoPropertyView.as_view()),
     path("photos/<int:pk>/", photo_views.RUDPhotoPropertyView.as_view()),
+    #path("search/filters/", property_views.FilterPropertyView.as_view()),
+    path("search/", property_views.SearchPropertyAIView.as_view()),
+    path("<int:pk>/reviews/", property_views.CreateListReviewPropertyView.as_view()),
+    path("<int:pk>/reviews/<int:review_pk>/", property_views.RUDReviewPropertyView.as_view()),
 ]
