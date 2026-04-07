419b305fcc8b3909bba365079109f17260ab0d6d
DevlTz <kauazin3339@gmail.com>
2026-04-07 18:07:09 -0300

    fix(views): cleanup unused imports and fix review detail lookup_url_kwarg

--- apps/properties/views/property_views.py
+++ apps/properties/views/property_views.py
@@ -1,33 +1,19 @@
-from apps.properties.serializers.reviews_serializers import ReviewsSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
 from rest_framework.views import APIView
-from rest_framework import generics, status, serializers
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.permissions import BasePermission
-from rest_framework.exceptions import PermissionDenied
-from apps.properties.serializers.property_serializers import PropertiesWriteSerializer, PropertiesReadSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny, BasePermission
 from apps.properties.models import Properties, Reviews
 from apps.properties.filters import PropertiesFilters
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from apps.properties.serializers.property_serializers import PropertiesWriteSerializer, PropertiesReadSerializer
+from apps.properties.serializers.reviews_serializers import ReviewsSerializer
+from apps.properties.permissions import IsAdvertiser, IsReviewOwner, IsPropertyOwner
 
 # C -> Create
 # R -> Read
 # U -> Update
 # D -> Delete
 
-class IsAdvertiser(BasePermission):
-    message = "You do not have permission to do this action. Please, change your account type to advertise!"
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and
-            request.user.user_type == "A"
-        )
-    
-class IsReviewOwner(BasePermission):
-    message = "You only can edit or delete your own reviews."
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user 
-    
 class CreateListReviewPropertyView(generics.ListCreateAPIView):
     serializer_class = ReviewsSerializer
 
@@ -50,30 +36,16 @@ class CreateListReviewPropertyView(generics.ListCreateAPIView):
         property_obj = get_object_or_404(Properties, pk=self.kwargs["pk"])
         serializer.save(user=self.request.user, property=property_obj)
 
-
 class RUDReviewPropertyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Properties.objects.all()
-    lookup_field = "pk"
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
+    lookup_url_kwarg = "review_pk"
 
     def get_permissions(self):
         if self.request.method in ["PUT", "PATCH", "DELETE"]:
-            return [IsAuthenticated(), IsPropertyOwner()]
+            return [IsAuthenticated(), IsReviewOwner()]
         return [AllowAny()]
 
-
-class IsPropertyOwner(BasePermission):
-    message = "You do not have permission to do this action."
-
-    def has_object_permission(self, request, view, obj):
-        if hasattr(obj, "owner"):
-            owner = obj.owner
-        elif hasattr(obj, "property"):
-            owner = obj.property.owner
-        else:
-            return False
-        return owner == request.user
-
-    
 class CreateListPropertyView(generics.ListCreateAPIView):
     queryset = Properties.objects.all().order_by("created_at")
     filterset_class = PropertiesFilters
@@ -106,11 +78,11 @@ class RUDPropertyView(generics.RetrieveUpdateDestroyAPIView):
         return [AllowAny()]
     
     def destroy(self, request, *args, **kwargs):
-        self.perform_destroy(self.get_object())
-
+        instance = self.get_object()
+        self.perform_destroy(instance)
         return Response({
-            "message": "Delete successfull!"
-        }, status=204)
+            "message": "Delete successful!"
+        }, status=status.HTTP_204_NO_CONTENT)
 
 class SearchPropertyAIView(APIView):
     pass
