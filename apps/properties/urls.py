c256900a56b679133baa1df1570dc8b8fe8ad617
DevlTz <kauazin3339@gmail.com>
2026-04-08 18:10:39 -0300

    refactor(properties): comment out search route pending AI implementation

--- apps/properties/urls.py
+++ apps/properties/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("<int:pk>/photos/", photo_views.UploadPhotoPropertyView.as_view()),
     path("photos/<int:pk>/", photo_views.RUDPhotoPropertyView.as_view()),
     #path("search/filters/", property_views.FilterPropertyView.as_view()),
-    path("search/", property_views.SearchPropertyAIView.as_view()),
+   #path("search/", property_views.SearchPropertyAIView.as_view()),
     path("<int:pk>/reviews/", property_views.CreateListReviewPropertyView.as_view()),
     path("<int:pk>/reviews/<int:review_pk>/", property_views.RUDReviewPropertyView.as_view()),
 ]
